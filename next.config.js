6c9bd771852c7adb4dd8aa9e204808304f9345e7
ORehman1 <obaid@piktorlabs.com>
2023-01-30 20:08:04 +0530

    proxy update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     conductor: {
       keyId: "fb4ca6c9-8762-44e6-ad17-98b6be97736d",
       keySecret: "v2c1qiBaoTpsKfTBlDmBZECzCwh11WA3f1Eq3jX1swe5HOqG",
-      serverUrl: "http://localhost:3000/api",
+      serverUrl: "https://conductor-6orq6si28-obaidpik.vercel.app/",
     },
     workflows:{
       checkout:`ob-checkout`,
